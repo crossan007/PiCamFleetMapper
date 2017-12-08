33727af7c523c5a6a1c4950b270c778e1446cb8a
Charles <crossan007@gmail.com>
2017-12-07 19:56:36 -0500

    remove client commandline opts

--- FleetMapper.py
+++ FleetMapper.py
@@ -427,9 +427,8 @@ def get_args():
         help="Use this when running on the core server to receive video streams")
 
     parser.add_argument(
-        '-c', '--camera', action='store',
-        choices=['rpicamsrc','v4l2src'],
-        help="Use this when capturing from a device.  Automatically finds core and streams on live")
+        '-c', '--camera', action='store_true',
+        help="Use this when capturing from a device.  Automatically finds core, downloads config, and streams on live")
 
     parser.add_argument(
         '-a', '--ip-address', action="store",
