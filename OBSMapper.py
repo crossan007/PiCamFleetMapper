6213a9a83db0ad79ee70718cd9998a7cf96cdad2
Charles <crossan007@gmail.com>
2018-05-27 18:22:28 -0400

    add obs plugin

--- OBSMapper.py
+++ OBSMapper.py
@@ -10,6 +10,14 @@ from lib.NetCamMasterAdvertisementService import NetCamMasterAdvertisementServic
 debug_mode = True
 
 
+def script_description():
+	global debug_mode
+	if debug_mode: print("Calling description")
+
+	return "<b>Remote Source Mapper</b>" + \
+		"<hr>" + \
+		"Map a set of GStreamer sources to named sources in OBS"
+
 def script_load(settings):
     master = NetCamMasterAdvertisementService(args.ip_address,54545)
     master.daemon = True
